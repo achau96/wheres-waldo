982f14dfb945094bb7fb1c8d6e7a7e0253bd650f
Austin Chau <austin.chau1@gmail.com>
2021-11-08 13:20:04 -0500

    Set up basic routing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
-  return <div className="home">This is the home page</div>;
+  return (
+    <div className="home">
+      This is the home page
+      <div>
+        <Link to="/image1">Image 1</Link>
+      </div>
+      <div>
+        <Link to="/image2">Image 2</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className="nav">
-      <div>Logo</div>
+      <div>
+        <Link to="/">Logo</Link>
+      </div>
       <div>Something here</div>
     </div>
   );
