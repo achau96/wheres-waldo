2794d0b24ede499e49bfcb4191d12992f99e2ad7
Austin Chau <austin.chau1@gmail.com>
2021-11-09 00:51:51 -0500

    Created custom cursor

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ What Did I learn?
 
 1. I was using an older version of react-router-dom in my shopping cart project.
    There were some changes which I had to revise.
+2. Image mapping. May need to install npm package, but as of this current version,
+   just uses html basics.
--- src/components/Image1.js
+++ src/components/Image1.js
@@ -5,7 +5,7 @@ const Image1 = () => {
     <div className="image">
       <div>This is where image 1 will go</div>
       <div>
-        <img src={image1} alt="image1" useMap="#testmap" />
+        <img src={image1} className="image1" alt="image1" useMap="#testmap" />
         <map name="testmap">
           <area
             shape="rect"
@@ -21,3 +21,8 @@ const Image1 = () => {
 };
 
 export default Image1;
+
+//function to get x and y coordinate of image
+// const image1 = document.querySelector('.image1')
+// image1.onclick = function(e){
+//   let x = e.pageX; console.log("X: " + e.offsetX + "Y: " + e.offsetY )}
